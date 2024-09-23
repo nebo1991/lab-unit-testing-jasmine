6045dd206bccdb2a90cf283550e9667b45c4c357
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-09-23 17:00:38 +0200

    fix: additional validation for divide function

--- src/divide.js
+++ src/divide.js
@@ -1,4 +1,7 @@
 function divide(num1, num2) {
   if ((!num1 && num1 !== 0) || (!num2 && num2 !== 0)) return undefined;
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return undefined;
+  }
   return num1 / num2;
 }
